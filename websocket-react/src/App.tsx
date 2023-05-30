f125d674c974385e1f801c490b74f7b17031b070
You-saku <yousaku.dev19@gmail.com>
2023-05-31 08:28:05 +0900

    :fire:delete: routing

--- websocket-react/src/App.tsx
+++ websocket-react/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Routing } from './components/Routing';
+///import { Routing } from './components/Routing';
 import { Home } from './pages/Home';
 import { Client } from './pages/Client';
 import { Streamer }  from './pages/Streamer';
@@ -8,7 +8,7 @@ import './App.css';
 export const App = () => {
   return (
     <div className="App">
-    <Routing />
+    {/* <Routing /> */}
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/client" element={<Client />} />
